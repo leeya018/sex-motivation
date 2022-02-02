5f1c0ebf180c18ae4fcd17451b9c10fe744dfae0
lee yahav <leeyahav018@gmail.com>
2022-02-02 23:43:12 +0200

    statt project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./style.css"
+
+function App() {
+
+  return (
+    <div className="center">
+      hello
+    </div>
+  )
+
+}
+
+
+export default App;
+

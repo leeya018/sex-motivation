ce413ae9d3773bebee93a4ea2bd1e76a81a673f0
lee yahav <leeyahav018@gmail.com>
2022-02-03 12:01:21 +0200

    invest is ready

--- src/components/Invest.jsx
+++ src/components/Invest.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 
+const HIT_VALUE = 1
+const PHONE_VALUE = 3
+const DATE_VALUE = 5
+const SEX_VALUE = 10
 
 export default function Invest({ updateMoney, money }) {
 
@@ -9,7 +13,11 @@ export default function Invest({ updateMoney, money }) {
   return <div>
     <h1>Invest</h1>
     <p>money: {money}</p>
-    <button onClick={() => updateMoney(money + 5)}>start</button><br />
+    <button onClick={() => updateMoney(money + HIT_VALUE)}>HIT</button><br />
+    <button onClick={() => updateMoney(money + PHONE_VALUE)}>PHONE</button><br />
+    <button onClick={() => updateMoney(money + DATE_VALUE)}>DATE</button><br />
+    <button onClick={() => updateMoney(money + SEX_VALUE)}>SEX</button><br />
+
     <button onClick={e => history.push("/expense")}>expense</button>
 
   </div>;
